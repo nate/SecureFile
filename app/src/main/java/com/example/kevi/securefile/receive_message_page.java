8ad7701dd1fa49c7e4bdfc82ea77b5f854be9a50
Joe <gayj@wit.edu>
2016-02-14 13:59:53 -0500

    started working on the receive message page

---
+++ app/src/main/java/com/example/kevi/securefile/receive_message_page.java
@@ -0,0 +1,19 @@
+package com.example.kevi.securefile;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class receive_message_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message_page);
+
+    }
+
+}
