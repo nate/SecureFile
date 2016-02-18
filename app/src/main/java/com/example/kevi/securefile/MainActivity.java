9e34bb466b96863326b983bd97335d7e2c84e4af
revinkey <revinkey@gmail.com>
2016-02-18 14:13:33 -0500

    intergrated a socket io chat that will be used later to send and recieve messgaes

--- app/src/main/java/com/example/kevi/securefile/MainActivity.java
+++ app/src/main/java/com/example/kevi/securefile/MainActivity.java
@@ -3,14 +3,10 @@ package com.example.kevi.securefile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,13 +45,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    //public void goToMessages(View view) {
-      //  Intent intent = new Intent(this, new_message_page.class);
-        //EditText editText = (EditText) findViewById(R.id.edit_message);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
-   //     startActivity(intent);
-   // }
+    public void goToMessages(View view) {
+        Intent intent = new Intent(this, receive_message_page.class);
+
+
+        startActivity(intent);
+    }
 
 }
 
